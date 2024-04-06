384c06278d77aa936b15d2569220c6cdb9f62a13
ruxandraRosu <40659997+ruxandraRosu@users.noreply.github.com>
2024-04-06 10:26:33 +0300

    Create Stats.java

---
+++ src/main/java/com/example/model/Stats.java
@@ -0,0 +1,25 @@
+package com.example.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Stats {
+
+    @JsonProperty("open")
+    private String open;
+    @JsonProperty("high")
+    private String high;
+    @JsonProperty("low")
+    private String low;
+    @JsonProperty("volume")
+    private String volume;
+
+
+}
