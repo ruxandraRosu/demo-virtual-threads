8f0192f0d5165060b2deec80dde57b9c9ed501fa
ruxandraRosu <40659997+ruxandraRosu@users.noreply.github.com>
2024-04-06 10:20:33 +0300

    Update KafkaService.java

--- src/main/java/com/example/service/KafkaService.java
+++ src/main/java/com/example/service/KafkaService.java
@@ -1,13 +1,17 @@
 package com.example.service;
 
 import com.example.dto.MyDto;
+import com.example.model.response.Trade;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @AllArgsConstructor
@@ -15,16 +19,34 @@ import org.springframework.stereotype.Service;
 public class KafkaService {
 
     public static final String TOPIC_NAME = "topic-name";
+    public static final String TOPIC_NAME_TRADES = "topic-trades";
     private KafkaTemplate<String, String> kafkaTemplate;
-    private ObjectMapper objectMapper;
+    private ObjectMapper mapper;
 
     public void sendMessage(MyDto value) {
         String message = "";
         try {
-            message = objectMapper.writeValueAsString(value);
+            message = mapper.writeValueAsString(value);
         } catch (JsonProcessingException e) {
-            log.error("Error serializing object", e);
+            log.error("Unable to serialzie object", e);
         }
         kafkaTemplate.send(TOPIC_NAME, message);
+    }
+
+    public void sendMessage(Trade trade) {
+        String message = "";
+        try {
+            message = mapper.writeValueAsString(trade);
+            kafkaTemplate.send(TOPIC_NAME_TRADES, message).get(500, TimeUnit.MILLISECONDS);
+        } catch (JsonProcessingException e) {
+            log.error("Unable to serialzie object", e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
