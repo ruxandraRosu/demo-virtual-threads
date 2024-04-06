3969f6e79be3f76c8a2e7727e2719b1cabf4fcdf
ruxandraRosu <40659997+ruxandraRosu@users.noreply.github.com>
2024-04-06 23:54:54 +0300

    Update KafkaService.java

--- src/main/java/com/example/service/KafkaService.java
+++ src/main/java/com/example/service/KafkaService.java
@@ -1,52 +1,44 @@
 package com.example.service;
 
-import com.example.dto.MyDto;
 import com.example.model.response.Trade;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.Uuid;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @AllArgsConstructor
 @Service
 public class KafkaService {
 
-    public static final String TOPIC_NAME = "topic-name";
     public static final String TOPIC_NAME_TRADES = "topic-trades";
     private KafkaTemplate<String, String> kafkaTemplate;
     private ObjectMapper mapper;
 
-    public void sendMessage(MyDto value) {
-        String message = "";
-        try {
-            message = mapper.writeValueAsString(value);
-        } catch (JsonProcessingException e) {
-            log.error("Unable to serialzie object", e);
-        }
-        kafkaTemplate.send(TOPIC_NAME, message);
-    }
 
     public void sendMessage(Trade trade) {
-        String message = "";
         try {
-            message = mapper.writeValueAsString(trade);
-            kafkaTemplate.send(TOPIC_NAME_TRADES, message).get(500, TimeUnit.MILLISECONDS);
+            String message = mapper.writeValueAsString(trade);
+            kafkaTemplate.send(TOPIC_NAME_TRADES, Uuid.randomUuid().toString(), message)
+                    .whenComplete(KafkaService::handleResult)
+                    .get();
         } catch (JsonProcessingException e) {
-            log.error("Unable to serialzie object", e);
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (TimeoutException e) {
+            log.error("Unable to serialize object", e);
+        } catch (ExecutionException | InterruptedException e) {
+            log.error("Exception publishing message", e);
             throw new RuntimeException(e);
         }
+    }
 
+    private static void handleResult(SendResult<String, String> result, Throwable e) {
+        if (e != null) {
+            throw new RuntimeException("Failed to publish message", e);
+        }
     }
 }
